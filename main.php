22452182e189e83834e3b86df0de0b5f8006ffa2
unknown <WindeusK>
2023-04-13 22:45:50 +0700

    13 April 2023 pt.2

--- main.php
+++ main.php
@@ -7,9 +7,38 @@
                 rel="stylesheet"
                 integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" 
                 crossorigin="anonymous">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300&display=swap" 
+              rel="stylesheet">     
+        
+        <style>
+            body {
+                font-family: "Noto Sans", 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif
+            }
+        </style>
     </head>
 
-    <body class="container-fluid">
-        <div class="jumbotron align-items-center">Judul</div>
+    <body class="bg-dark">
+        <div class="container-fluid">
+
+        <div class="d-flex justify-content-center align-self-center">
+            
+            <div class="jumbotron pt-4 text-white">
+                <center>
+                <h1>Biblio Library</h1>
+                <h4>Project UTS</h4>
+                </center>
+            </div>
+        </div>
+
+        <div class="d-flex justify-content-center align-self-center">
+        <button type="button" class="btn btn-light rounded-lg mt-5" href="daftarBuku.php">
+                Daftar Buku
+        </button>
+        </div>
+
+
+        </div>
     </body>
 </html>
