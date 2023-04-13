ae46e59f624130494b1fc33d4eab91dec62eaf8d
unknown <WindeusK>
2023-04-13 21:24:45 +0700

    13 April 2023

--- main.php
+++ main.php
@@ -2,29 +2,14 @@
 
 <html>
     <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" 
-            rel="stylesheet" 
-            integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" 
-            crossorigin="anonymous">
+        <meta charset="utf-8">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css"
+                rel="stylesheet"
+                integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" 
+                crossorigin="anonymous">
     </head>
 
-    <body>
-        <?php require 'php\connect_db.php'; ?>
-        <div class="container-fluid pt-2 bg-dark text-white">
-            <h1>This is a heading</h1>
-            <p>This is a paragraph</p>
-            <div class="table pt-2">
-                <table>
-                    <th>
-                        <div class="card">
-                            <p>Hello World!</p>
-                        </div>
-                    </th>
-                </table>
-            </div>
-            
-        </div>
+    <body class="container-fluid">
+        <div class="jumbotron align-items-center">Judul</div>
     </body>
 </html>
